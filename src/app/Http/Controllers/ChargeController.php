122025b0ea7e79547e1ebd1f46cfc9f98e36463a
Michel Lima <michelpl@yahoo.com.br>
2022-12-23 17:53:16 -0300

    :sparkles: Create chage model

---
+++ src/app/Http/Controllers/ChargeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreChargeRequest;
+use App\Http\Requests\UpdateChargeRequest;
+use App\Models\Charge;
+
+class ChargeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreChargeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChargeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Charge  $charge
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Charge $charge)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Charge  $charge
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Charge $charge)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateChargeRequest  $request
+     * @param  \App\Models\Charge  $charge
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateChargeRequest $request, Charge $charge)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Charge  $charge
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Charge $charge)
+    {
+        //
+    }
+}
